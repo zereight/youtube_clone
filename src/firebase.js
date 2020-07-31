3fd22bbf45911794872d16b00cacea039d2e68ae
zereight <zereight@zereightui-MacBookPro.local>
2020-07-31 22:13:39 +0900

    react init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCeSMNF851DeN_34dWy_EYuq-DEFAuTN9s",
+  authDomain: "clone-387a3.firebaseapp.com",
+  databaseURL: "https://clone-387a3.firebaseio.com",
+  projectId: "clone-387a3",
+  storageBucket: "clone-387a3.appspot.com",
+  messagingSenderId: "601493749936",
+  appId: "1:601493749936:web:a549aeca951590c7c2fb75",
+  measurementId: "G-B85SJ2W644",
+};
